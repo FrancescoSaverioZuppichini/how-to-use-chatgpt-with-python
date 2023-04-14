a9f2ca6a9ae23b925944876726cdf1648ac67c9d
Francesco Zuppichini <francescozuppichini@Francescos-MBP.lan>
2023-04-14 20:21:30 +0200

    rich markdown formatter + ctrl+c handler

--- cli.py
+++ cli.py
@@ -1,23 +1,45 @@
 import openai
 import os
+from rich.console import Console
+from rich.markdown import Markdown
 
+console = Console()
 
 messages = [
     # system message first, it helps set the behavior of the assistant
     {"role": "system", "content": "You are a helpful assistant."},
 ]
 
-while True:
-    message = input("👨‍💻: ")
-    if message:
-        messages.append(
-            {"role": "user", "content": message},
-        )
-        # doc is here https://platform.openai.com/docs/guides/chat/chat-vs-completions?utm_medium=email&_hsmi=248334739&utm_content=248334739&utm_source=hs_email
-        chat_completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages
-        )
-    # get the reply
-    reply = chat_completion.choices[0].message.content
-    print(f"🤖: {reply}")
-    messages.append({"role": "assistant", "content": reply})
+
+def handle_keyboard_interrupt(n: int = 0):
+    try:
+        response = input("\nAre you sure you want to exit? (Y/N): ")
+        if response.upper() == "Y":
+            exit(0)
+        else:
+            return
+    except KeyboardInterrupt:
+        # we will allow two spammed ctrl+c, then bye bye
+        if n < 1:
+            handle_keyboard_interrupt(n + 1)
+        return
+
+
+try:
+    while True:
+        message = input("👨‍💻: ")
+        if message:
+            messages.append(
+                {"role": "user", "content": message},
+            )
+            # doc is here https://platform.openai.com/docs/guides/chat/chat-vs-completions?utm_medium=email&_hsmi=248334739&utm_content=248334739&utm_source=hs_email
+            chat_completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo", messages=messages
+            )
+        # get the reply
+        reply = chat_completion.choices[0].message.content
+        print("🤖: ", end="")
+        console.print(Markdown(reply))
+        messages.append({"role": "assistant", "content": reply})
+except KeyboardInterrupt:
+    handle_keyboard_interrupt()
